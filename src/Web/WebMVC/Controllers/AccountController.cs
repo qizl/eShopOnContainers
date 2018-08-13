3bf2bc2f3818d609a3738b676807fad92058a768
qizl <q@qizl.cn>
2018-08-14 00:10:50 +0800

    -

--- src/Web/WebMVC/Controllers/AccountController.cs
+++ src/Web/WebMVC/Controllers/AccountController.cs
@@ -1,11 +1,10 @@
 ﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http.Authentication;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Microsoft.eShopOnContainers.WebMVC.Controllers
 {
